3452947c39b7638ffe3ca4848cfcb66b57f2a0a8
David <electromandarin@gmail.com>
2023-02-07 14:48:47 +0200

    feat: scroll anim

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,9 +1,22 @@
 import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 import SquareBlock from '../squareBlock';
 import Carousel from './carousel';
 import styles from './styles.module.scss';
 
 const Skills = () => {
+  const [scroll, setScroll] = useState(window.scrollY);
+
+  const handleScroll = () => {
+    setScroll(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <SquareBlock
       borderRadius={75}
@@ -12,8 +25,18 @@ const Skills = () => {
       className={styles.tech}
     >
       <div className={styles.heading}>
-        <h1 className={styles.crtText}>Front-End</h1>
-        <h1 className={styles.crtText}>Developer</h1>
+        <h1
+          className={styles.crtText}
+          style={{ transform: `translateX(${scroll * 0.22}px)` }}
+        >
+          Front-End
+        </h1>
+        <h1
+          className={styles.crtText}
+          style={{ transform: `translateX(-${scroll * 0.22}px)` }}
+        >
+          Developer
+        </h1>
       </div>
 
       <h2 className={clsx(styles.techTitle, styles.crtText)}>
